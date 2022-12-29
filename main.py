bcee1316ae7047b497a17346211c17ecaff4a8e2
Павел Леонтьев <fionit-2007@mail.ru>
2022-12-29 23:27:29 +0300

    Connect main.py with start_game_bunner.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+"""Module where character is created. Training gameplay."""
+
 from random import randint
 
+from typing import Optional
 
-def attack(char_name: str, char_class: str) -> str:
+from graphic_arts.start_game_banner import run_screensaver
+
+
+def attack(char_name: str, char_class: str) -> Optional[str]:
+    """Generate attack points."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,7 +21,8 @@ def attack(char_name: str, char_class: str) -> str:
     return None
 
 
-def defence(char_name: str, char_class: str) -> str:
+def defence(char_name: str, char_class: str) -> Optional[str]:
+    """Generate defence points."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,7 +32,8 @@ def defence(char_name: str, char_class: str) -> str:
     return None
 
 
-def special(char_name: str, char_class: str) -> str:
+def special(char_name: str, char_class: str) -> Optional[str]:
+    """Generate special bonus points."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -36,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Suggest trainining mode or skip taining mode."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -49,7 +59,7 @@ def start_training(char_name: str, char_class: str) -> str:
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd = None
     while cmd != 'skip':
-        cmd: str = input('Введи команду: ')
+        cmd = input('Введи команду: ')
         if cmd == 'attack':
             print(attack(char_name, char_class))
         if cmd == 'defence':
@@ -60,10 +70,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Create character."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
-        char_class: str = input('Введи название персонажа, за которого хочешь '
+        char_class = input('Введи название персонажа, за которого хочешь '
                                 'играть: Воитель — warrior, Маг — mage, '
                                 'Лекарь — healer: ')
         if char_class == 'warrior':
@@ -75,13 +86,17 @@ def choice_char_class() -> str:
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. Черпает силы из '
                   'природы, веры и духов.')
-        approve_choice: str = input('Нажми (Y), чтобы подтвердить выбор, или '
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или '
                                     'любую другую кнопку, чтобы выбрать '
                                     'другого персонажа ').lower()
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    """Greet player. Suggest name and character creation.
+    Suggest training mode.
+    """
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
